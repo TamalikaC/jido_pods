37a1c8948bac239190ea3347ad407050cec75696
Mark <stauber@stanford.edu>
2018-01-26 10:47:02 -0800

    first

---
+++ MapsyncLib.framework/Headers/MapsyncLib.h
@@ -0,0 +1,19 @@
+//
+//  MapsyncLib.h
+//  MapsyncLib
+//
+//  Created by Jaeyong Sung on 1/17/18.
+//  Copyright © 2018 Mapsync. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapsyncLib.
+FOUNDATION_EXPORT double MapsyncLibVersionNumber;
+
+//! Project version string for MapsyncLib.
+FOUNDATION_EXPORT const unsigned char MapsyncLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapsyncLib/PublicHeader.h>
+
+
