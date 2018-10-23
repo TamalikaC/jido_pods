6e91c39120bf181fdca50139b0b4937b341f69ca
Devin <dhasl002@odu.edu>
2018-10-23 17:12:52 -0400

    pod 10/23 5:12

--- JidoMaps.framework/Headers/JidoMaps-Swift.h
+++ JidoMaps.framework/Headers/JidoMaps-Swift.h
@@ -276,12 +276,14 @@ SWIFT_CLASS("_TtC8JidoMaps3Map")
 SWIFT_CLASS("_TtC8JidoMaps8MapAsset")
 @interface MapAsset : NSObject
 @property (nonatomic, copy) NSString * _Nonnull assetID;
+@property (nonatomic, copy) NSString * _Nonnull uuid;
 @property (nonatomic) SCNVector3 position;
 @property (nonatomic) float orientation;
 @property (nonatomic) float confidence;
 @property (nonatomic, copy) NSString * _Nonnull matrix;
 @property (nonatomic) BOOL isDownloaded;
 @property (nonatomic, strong) SCNNode * _Nullable assetNode;
+@property (nonatomic) float assetScale;
 - (nonnull instancetype)init:(NSString * _Nonnull)assetID :(SCNVector3)position :(float)orientation;
 - (BOOL)isEqual:(id _Nullable)object SWIFT_WARN_UNUSED_RESULT;
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
